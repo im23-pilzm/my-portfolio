73cc287a3f1f7f5d7f004596af3d58337c3bdacd
Maximillian Pilz <pilzm@bzz.ch>
2025-03-15 15:20:33 +0100

    Implemented routes and started header-layout

---
+++ app/impressum/page.jsx
@@ -0,0 +1,3 @@
+export default function page(){
+    return <div>Welcome to the impressum page!</div>;
+}
---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function main_page(){
+    return <div>Welcome to the main page!</div>;
+}
